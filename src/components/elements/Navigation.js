55ddb3d348bfc93698054c48e85a935a8dad1ec6
Kinuthia254 <muchenekevin@gmail.com>
2020-04-05 12:11:29 +0300

    first commit

---
+++ src/components/elements/Navigation.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Link} from '@reach/router';
+
+import {StyledNavigation} from '../styles/StyledNavigation';
+
+import PropTypes from 'prop-types';
+
+const Navigation = ({movie}) =>(
+    <StyledNavigation>
+        <div className="navigation-content">
+
+            <Link to="/">
+                <p>Home</p>
+            
+            </Link>
+            <p>|</p>
+            <p>{movie}</p>
+        </div>
+    </StyledNavigation>
+) ;
+
+Navigation.protoTypes ={
+    movie: PropTypes.string,
+}
+
+export default Navigation;
